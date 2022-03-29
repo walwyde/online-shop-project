cd5c9f08689600d737e78c15dfec5a70a6c0aa22
walwyde <walwyde@gmail.com>
2022-03-29 12:41:22 +0100

    refactored product model code

--- models/productModel.js
+++ models/productModel.js
@@ -1,35 +1,36 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
-);
-
-module.exports = class Product {
-  constructor(t) {
-    this.title = t;
-  }
-  save() {
-   fs.readFile(p, (err, fileContent) => {
-     let products = []
-     if (!err) {
-       products = JSON.parse(fileContent)
-      }
-      products.push(this)
-     fs.writeFile(p, JSON.stringify(products), err => {
-       console.log(err)
-     })
-   })
-  }
-  static fetchAll(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([])
-      }
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+const getProductFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([])
+    }
+    else{
       cb(JSON.parse(fileContent))
-    })
-   
+    }
+  })
+}
+
+module.exports = class product {
+  constructor (t) {
+    this.title = t
   }
-};
+  save () {
+    getProductFromFile(products => {
+      products.push(this)
+      
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.error(err)
+      })
+    })
+     
+
+      
+  }
+
+  static fetchAll(cb) {
+   getProductFromFile(cb)
+  }
+}
